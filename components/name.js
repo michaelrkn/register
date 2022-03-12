4223ff119f9671c8890736f5abe6a2d6725a0d5e
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2022-03-12 14:29:21 -0600

    create basic printing form

---
+++ components/name.js
@@ -0,0 +1,40 @@
+export default function Name(props) {
+  if (props) {
+    const type = props.type
+  }
+  return (
+    <div>
+      <div className="row">
+        <div className="col-2">
+          <label htmlFor={`"title${type}"`}>{type} Title</label>
+          <select id={`"title${type}"`} name={`"title${type}"`}>
+            <option></option>
+            <option value="Mr.">Mr.</option>
+            <option value="Ms.">Ms.</option>
+            <option value="Mrs.">Mrs.</option>
+            <option value="Miss”">Miss</option>
+          </select>
+        </div>
+        <div className="col">
+          <label htmlFor={`"firstName${type}"`}>{type} First Name</label>
+          <input id={`"firstName${type}"`} name={`"firstName${type}"`} type="text" />
+        </div>
+        <div className="col">
+          <label htmlFor={`"lastName${type}"`}>{type} Last Name</label>
+          <input id={`"lastName${type}"`} name={`"lastName${type}"`} type="text" />
+        </div>
+        <div className="col-3">
+          <label htmlFor={`"suffix${type}"`}>{type} Suffix</label>
+          <select id={`"suffix${type}"`} name={`"suffix${type}"`}>
+            <option></option>
+            <option value="Jr.">Jr.</option>
+            <option value="Sr.">Sr.</option>
+            <option value="II”">II</option>
+            <option value="III">III</option>
+            <option value="IV">IV</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/[state].js
+++ pages/[state].js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Address from '../components/address'
+import Name from '../components/name'
 import { useRouter } from 'next/router'
 import statesInfo from '../public/states-info'
 
@@ -31,19 +33,92 @@ export default function Home() {
             <div>
               <h2>Online</h2>
 
-              <p>If you have {stateInfo.ovrRequirements}, you can register online.</p>
+              <p>If you have {stateInfo.ovrRequirements}, you can register online with the state.</p>
 
               {stateInfo.ovrNotes && <p>Click the button below. <strong>{stateInfo.ovrNotes}</strong></p>}
 
               <p><a href={`${stateInfo.ovrLink}`} target="_blank" rel="noreferrer" className="button primary">Register Online</a></p>
-
-              <h2>By Mail</h2>
             </div>
           }
 
           {!stateInfo.specialCase &&
             <div>
-              <p>[add system for mail registration]</p>
+              {stateInfo.ovrAvailable
+                ? <h2>By Mail</h2>
+                : <p>{state} does not have online voter registration, so you must register by mail (or in-person).</p>
+              }
+              <p>Fill out the form below.</p>
+              <p>Either you can print it out, or we can print it and mail it to you.</p>
+              <p>Then sign it and mail it in.</p>
+              <form>
+                <fieldset>
+                  <div className="row">
+                    <div className="col">
+                      <label htmlFor="citizen">
+                      {/* the checkbox state isn't properly tracked, fixme pls */}
+                        <input id="citizen" name="citizen" type="checkbox" />
+                        I am a U.S. citizen
+                      </label>
+                    </div>
+                  </div>
+                  <Name />
+                  <div className="row">
+                    <div className="col">
+                      <label htmlFor="previousName">
+                      {/* the checkbox state isn't properly tracked, fixme pls */}
+                        <input id="previousName" name="previousName" type="checkbox" />
+                        I have changed my name
+                      </label>
+                    </div>
+                  </div>
+                  <Address type="Home" />
+                  <div className="row">
+                    <div className="col">
+                      <label htmlFor="hasMailingAddress">
+                      {/* the checkbox state isn't properly tracked, fixme pls */}
+                        <input id="hasMailingAddress" name="hasMailingAddress" type="checkbox" />
+                        I get mail at a different address
+                      </label>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <label htmlFor="previousRegistration">
+                      {/* the checkbox state isn't properly tracked, fixme pls */}
+                        <input id="previousRegistration" name="previousRegistration" type="checkbox" />
+                        I previously registered at a different address
+                      </label>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <label htmlFor="birthDate">Date of Birth</label>
+                      <input id="birthDate" name="birthDate" type="date" />
+                    </div>
+                    <div className="col">
+                      <label htmlFor="idNumber">Last 4 of Your Social Security Number</label>
+                      <input id="idNumber" name="idNumber" type="text" />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col">
+                      <div>
+                        <label htmlFor="receiveByEmail">
+                          <input type="radio" id="receiveByEmail" name="formMedium" value="email" />
+                          I have access to a printer. I will print, sign, and mail my application.
+                        </label>
+                      </div>
+                      <div>
+                        <label htmlFor="receiveByMail">
+                          <input type="radio" id="receiveByMail" name="formMedium" value="mail" />
+                          I don't have a printer. Print the form and mail it to me. Then I will sign and mail it in.
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row"><button type="submit">Print My Application</button></div>
+                </fieldset>
+              </form>
             </div>
           }
 
