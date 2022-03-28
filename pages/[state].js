748d625fc7767a83e2859c502536ec830d150eeb
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2022-03-27 21:44:58 -0500

    fix ovrAvailable not defined error

--- pages/[state].js
+++ pages/[state].js
@@ -66,7 +66,7 @@ export default function Home(props) {
     setPreviousRegistration(!hasPreviousRegistration)
   }
 
-  const [mailFormShowing, setMailFormShowing] = useState(!stateOnlineInfo.ovrAvailable && !stateOnlineInfo.specialCase)
+  const [mailFormShowing, setMailFormShowing] = useState(!stateOnlineInfo.specialCase && !stateOnlineInfo.ovrAvailable)
   const showMailForm = () => {
     setMailFormShowing(true)
   }
