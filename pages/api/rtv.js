4b4dc6aeb67110085801a3e366a9f425208780c0
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2022-03-30 22:22:04 -0500

    refactor rtv proxy for readability

--- pages/api/rtv.js
+++ pages/api/rtv.js
@@ -1,26 +1,34 @@
 import { withSentry, captureException } from '@sentry/nextjs';
 
+const fetchRtv = async (req, path) => {
+  const options = req.method === 'GET' ? {} : {
+    method: req.method,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req.body)
+  }
+  const url = 'https://register.rockthevote.com' + path
+  return await fetch(url, options)
+}
+
+const sendErrorToSentry = async (response) => {
+  const bodyText = await response.text()
+  try {
+    throw new Error("Rock The Vote returned HTTP error code " + response.status + ": " + bodyText)
+  } catch(e) {
+    captureException(e)
+  }
+}
+
 const handler = async (req, res) => {
   const { path } = req.query
   if (path) {
-    const options = req.method === 'GET' ? {} : {
-      method: req.method,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(req.body)
-    }
-    const url = 'https://register.rockthevote.com' + path
-    const response = await fetch(url, options)
-    res.status(response.status).send(response.body)
+    const response = await fetchRtv(req, path)
     if (!response.ok) {
-      try {
-        const body = await response.text()
-        throw new Error("Rock The Vote returned HTTP error code " + response.status + ": " + body)
-      } catch(e) {
-        captureException(e)
-      }
+      sendErrorToSentry(response)
     }
+    res.status(response.status).send(response.body)
   } else {
     res.status(400).send("Invalid path: " + path)
   }
